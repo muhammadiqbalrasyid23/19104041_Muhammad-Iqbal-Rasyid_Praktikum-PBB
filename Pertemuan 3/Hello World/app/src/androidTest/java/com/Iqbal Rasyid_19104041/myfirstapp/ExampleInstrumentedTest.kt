b2efe9b0794141e7e9cfb04bdeed54340b4e5a78
muhammadiqbalrasyid23 <72425140+muhammadiqbalrasyid23@users.noreply.github.com>
2021-12-08 15:39:36 +0700

    Update ExampleInstrumentedTest.kt

--- Pertemuan 3/Hello World/app/src/androidTest/java/com/Iqbal Rasyid_19104041/myfirstapp/ExampleInstrumentedTest.kt
+++ Pertemuan 3/Hello World/app/src/androidTest/java/com/Iqbal Rasyid_19104041/myfirstapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.fauziyah_19104033.myfirstapp
+package com.iqbal_19104041.myfirstapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fauziyah_19104033.myfirstapp", appContext.packageName)
+        assertEquals("com.iqbal_19104041.myfirstapp", appContext.packageName)
     }
 }
