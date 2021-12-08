da41cfa14c5d86e639b5a69b4e3745115f7823d1
muhammadiqbalrasyid23 <72425140+muhammadiqbalrasyid23@users.noreply.github.com>
2021-12-08 15:27:23 +0700

    Update ExampleInstrumentedTest.kt

--- Pertemuan 3/Hello World ITTELKOM/app/src/androidTest/java/com/Iqbal Rasyid_19104041/helloittelkompurwokerto/ExampleInstrumentedTest.kt
+++ Pertemuan 3/Hello World ITTELKOM/app/src/androidTest/java/com/Iqbal Rasyid_19104041/helloittelkompurwokerto/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fauziyah_19104033.helloittelkompurwokerto", appContext.packageName)
+        assertEquals("com.iqbal_19104041.helloittelkompurwokerto", appContext.packageName)
     }
 }
