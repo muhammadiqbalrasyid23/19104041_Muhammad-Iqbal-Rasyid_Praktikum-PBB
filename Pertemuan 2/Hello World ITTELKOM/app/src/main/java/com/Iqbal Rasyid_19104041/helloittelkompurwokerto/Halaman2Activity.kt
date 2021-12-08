449cd6283cdd78f07fde68dd15d4fc5f104609fd
muhammadiqbalrasyid23 <72425140+muhammadiqbalrasyid23@users.noreply.github.com>
2021-12-08 15:33:56 +0700

    Update Halaman2Activity.kt

--- Pertemuan 2/Hello World ITTELKOM/app/src/main/java/com/Iqbal Rasyid_19104041/helloittelkompurwokerto/Halaman2Activity.kt
+++ Pertemuan 2/Hello World ITTELKOM/app/src/main/java/com/Iqbal Rasyid_19104041/helloittelkompurwokerto/Halaman2Activity.kt
@@ -1,4 +1,4 @@
-package com.fauziyah_19104033.helloittelkompurwokerto
+package com.iqbal_19104041.helloittelkompurwokerto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
