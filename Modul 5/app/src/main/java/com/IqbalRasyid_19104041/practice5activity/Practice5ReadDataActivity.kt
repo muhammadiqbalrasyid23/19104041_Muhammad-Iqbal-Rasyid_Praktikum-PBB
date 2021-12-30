35ed872f0625b033347b77c7638852de6927f015
muhammadiqbalrasyid23 <72425140+muhammadiqbalrasyid23@users.noreply.github.com>
2021-12-30 17:24:07 +0700

    Add files via upload

---
+++ Modul 5/app/src/main/java/com/IqbalRasyid_19104041/practice5activity/Practice5ReadDataActivity.kt
@@ -0,0 +1,24 @@
+package com.IqbalRasyid_19104041.practice5activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityReadData : AppCompatActivity(){
+
+    private lateinit var lblProdiSaya : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_read_data)
+        
+        lblProdiSaya = findViewById(R.id.lblProdiSaya)
+
+        val prodi = intent.getStringExtra(EXTRA_PRODI)
+        lblProdiSaya.text = "Prodi Saya Adalah $prodi"
+    }
+
+    companion object {
+        const val EXTRA_PRODI = "extra_prodi"
+    }
+
+}
